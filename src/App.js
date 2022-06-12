38bba32509ea4efe7218d5d28dd9fa6132c05104
Pratham kumar <prathamkumar6699@gmail.com>
2022-06-12 19:25:31 +0530

    news categories in navbar is added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <>
+      <Router>
       <NavBar/>
-      <News/>
+    <Routes>
+      <Route exact path="/home" element={<News key = "" country ="in" category =''/>}></Route>
+      <Route exact path="/business" element={<News key = "business" country ="in" category ='business'/>}></Route>
+      <Route exact path="/entertainment" element={<News key = "entertainment" country ="in" category ='entertainment'/>}></Route>
+      <Route exact path="/general" element={<News key = "general" country ="in" category ='general'/>}></Route>
+      <Route exact path="/health" element={<News key = "health" country ="in" category ='health'/>}></Route>
+      <Route exact path="/science" element={<News key = "science" country ="in" category ='science'/>}></Route>
+      <Route exact path="/sports" element={<News key = "sports" country ="in" category ='sports'/>}></Route>
+      <Route exact path="/technology" element={<News key = "technology" country ="in" category ='technology'/>}></Route>
+    </Routes>
+  </Router>
       </>
     )
   }
